423132ba4c07ed34a545ac7b7606cf1b0cb26e55
davidwpowell <david.w.powell90@gmail.com>
2018-03-07 22:03:54 -0500

    Create controller for file uploads

---
+++ src/main/java/com/prototype/restservice/controller/FileUploadController.java
@@ -0,0 +1,34 @@
+package com.prototype.restservice.controller;
+
+import com.prototype.restservice.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class FileUploadController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    FileUploadController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @GetMapping("/upload")
+    public String displayPage() {
+        return "uploadForm";
+    }
+
+    @PostMapping("/upload")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+        return "redirect:/upload";
+    }
+}
